e827cbcd8c88c2de0db672feece2e98c6d9e3620
Joe Kim <josephkim0224@gmail.com>
2017-12-05 10:39:25 -0500

    add change and click handlers for CreateTask form

--- src/components/view/CreateTask.js
+++ src/components/view/CreateTask.js
@@ -2,11 +2,45 @@ import React, { Component } from 'react'
 
 class CreateTask extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      task: {
+        title: '',
+        description: '',
+        category: ''
+      }
+    }
+  }
+
+  updateTask(event) {
+    event.preventDefault()
+
+    let updated = Object.assign({}, this.state.task)
+    updated[event.target.id] = event.target.value
+    this.setState({
+      task: updated
+    })
+  }
+
+  submitTask(event) {
+    event.preventDefault()
+
+    console.log(this.state.task)
+  }
+
   render(){
 
     return (
       <div>
-        CreateTask
+        <h2>CreateTask</h2>
+        <input id="title" onChange={this.updateTask.bind(this)} type="text" placeholder="Title" />
+        <br />
+        <input id="description" onChange={this.updateTask.bind(this)} type="text" placeholder="Description" />
+        <br />
+        <input id="category" onChange={this.updateTask.bind(this)} type="text" placeholder="Category" />
+        <br />
+        <button onClick={this.submitTask.bind(this)}>Submit</button>
       </div>
     )
   }
