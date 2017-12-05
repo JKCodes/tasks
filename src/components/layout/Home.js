af2ea9497e0fe4a900a87256180bb09455374050
Joe Kim <josephkim0224@gmail.com>
2017-12-05 10:27:33 -0500

    create Tasks container

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Tasks } from '../containers'
 
 class Home extends Component {
   render(){
     return (
       <div>
         Home
+        <Tasks />
       </div>
     )
   }
