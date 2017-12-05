4743740e5500dd7ece00467959c725760d5b4491
Joe Kim <josephkim0224@gmail.com>
2017-12-05 15:55:43 -0500

    link account route on app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 var mongoose = require('mongoose')
+var sessions = require('client-sessions')
 require('dotenv').config()
 
 var dbUrl = process.env.DB_URL
@@ -16,44 +17,54 @@ mongoose.connect(dbUrl, function(err, res) {
   }
 })
 
-var index = require('./routes/index');
-var api = require('./routes/api');
-var twilio = require('./routes/twilio');
+var routes = require('./routes/index')
+var api = require('./routes/api')
+var twilio = require('./routes/twilio')
+var account = require('./routes/account')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hjs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', index);
-app.use('/api', api);
-app.use('/twilio', twilio);
+app.use(sessions({
+  cookieName: 'session',
+  secret: process.env.SESSION_SECRET,
+  duration: 24*60*60*1000, // 1 day
+  activeDuration:30*60*1000
+}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes)
+app.use('/api', api)
+app.use('/twilio', twilio)
+app.use('/account', account)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-module.exports = app;
+module.exports = app
