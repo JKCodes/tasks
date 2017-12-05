4625b3c020a8aa97559965d9295da5e66285c82d
Joe Kim <josephkim0224@gmail.com>
2017-12-05 10:33:43 -0500

    create Tasks container and CreateTask component

--- src/components/containers/Tasks.js
+++ src/components/containers/Tasks.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import { APIManager } from '../../utils'
+import { CreateTask } from '../view'
 
 class Task extends Component {
+
+  componentDidMount() {
+    APIManager.get('/api/task', null)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
 
   render(){
 
     return (
       <div>
-        Task Container
+        Tasks Container
+        <CreateTask />
       </div>
     )
   }
---
+++ src/components/view/CreateTask.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class CreateTask extends Component {
+
+  render(){
+
+    return (
+      <div>
+        CreateTask
+      </div>
+    )
+  }
+}
+
+export default CreateTask
+
+
