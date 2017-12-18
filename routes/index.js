87425a2171f158de5b23123ce876a87d870b58d4
Joe Kim <josephkim0224@gmail.com>
2017-12-18 17:01:16 -0500

    server-side load profile page

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,8 @@ var ReactDOMServer = require('react-dom/server');
 var serverapp = require('../public/build/es5/serverapp');
 var store = require('../public/build/es5/stores')
 var Home = require('../public/build/es5/components/layout/Home')
+var Split = require('../public/build/es5/components/layout/Split')
+var ProfileLayout = require('../public/build/es5/components/layout/ProfileLayout')
 
 var controllers = require('../controllers')
 
@@ -71,6 +73,57 @@ router.get('/', function(req, res, next) {
   .catch(function(err) {
     console.log('Not logged in')
   })  
+});
+
+router.get('/:page/:id', function(req, res, next) {
+  var page = req.params.page
+  var id = req.params.id
+
+  var initialStore = null
+  var reducers = {}
+
+  if (page == 'api') {
+    next()
+
+    return
+  }
+
+  if (page == 'profile') {
+    controllers.profile.getById(id, false)
+    .then(function(profile) {
+      console.log(profile)
+
+      var id = profile.id
+
+      reducers['profile'] = {
+        id: profile
+      }
+    })
+    .then(function() {
+      initialStore = store.configureStore(reducers)
+
+      var routes = {
+        path: '/profile/:id',
+        component: serverapp,
+        initial: initialStore,
+        indexRoute: {
+          component: ProfileLayout
+        }
+      }
+
+      return matchRoutes(req, routes)
+    })
+    .then(function(renderProps) {
+      var html = ReactDOMServer.renderToString(React.createElement(ReactRouter.RouterContext, renderProps))
+      res.render('index', { react: html, preloadedState: JSON.stringify(initialStore.getState()) });
+    })
+    .catch(function(err) {
+      console.log(err)
+    })
+  }
+
+  
+
 });
 
 module.exports = router;
